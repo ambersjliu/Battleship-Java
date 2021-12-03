5f8ad5d949bc81e51ef62ed3416e7c4b6df4d66e
Amber Liu <amber.sj.liu@gmail.com>
2021-12-03 17:17:59 -0500

    board now initialized with hit and taken set to false, getter method for points added

--- battleship/Main.java
+++ battleship/Main.java
@@ -11,34 +11,29 @@ public class Main {
     AI ai;
 
     public static void main(String[] args) {
-        System.out.println("hello jjjwod");
 /*         if (low ai){
             ai=new DumbAI()
         }else{
             ai=new SmartAI()
         } */
-        //Board testBoard = new Board(10);
+        Board testBoard = new Board(10);
 
         //System.out.println(testBoard.get)
 
-        Point[][] testBoard = new Point[10][10];
+        //Point[][] testBoard = new Point[10][10];
         
 
-         for(int i = 0; i<10; i++){
-            for(int j = 0; j < 10; j++){
-                testBoard[i][j] = new Point(false, false);
-            }
-        }
+
 
         //"ships"
         for(int i = 2; i < 7; i++){
-            testBoard[i][1].setIsTaken(true);
-            testBoard[1][i].setIsTaken(true);
+            testBoard.getPoint(i, 1).setIsTaken(true);
+            testBoard.getPoint(1, i).setIsTaken(true);
         }
 
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
-                if(testBoard[i][j].getIsTaken() == false){
+                if(testBoard.getPoint(i, j).getIsTaken() == false){
                     System.out.print("o ");
                 }else{
                     System.out.print("* ");
--- battleship/Model/Board.java
+++ battleship/Model/Board.java
@@ -7,6 +7,17 @@ public class Board {
     public Board(int size){
         boardSize = size;
         points = new Point[boardSize][boardSize];
-        
+
+        //initialize board with hit and taken all as false
+        for(int i = 0; i<boardSize; i++){
+            for(int j = 0; j < 10; j++){
+                points[i][j] = new Point(false, false);
+            }
+        }
+    }
+
+    //getter method for individiual coordinates
+    public Point getPoint(int pointX, int pointY){
+        return points[pointX][pointY];
     }
 }
