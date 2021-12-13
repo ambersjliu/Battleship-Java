e4b3a3a474e890124317a1009b1d0eb8d844c7bd
Marchy <elaineyang1212@gmail>
2021-12-13 17:08:18 -0500

    turn based skeleton code

--- battleship/Main.java
+++ battleship/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
         Board testBoard = new Board(Constants.boardSize);
 
+        testBoard.getPoint(1, 1).getIsHit();
 
         Ship carrier = new Ship(testBoard, "Carrier", 5, testBoard.getPoint(6, 2), "DOWN");
 
@@ -32,10 +33,17 @@ public class Main {
             // convert inputted coord into an array of 2 ints
             
             String coord = input.nextLine().toUpperCase(); //get user input
+            /* char[] coordArray = coord.toCharArray(); //convert
+            int ascii = Character.toUpperCase(coordArray[0]);
+            int[] numcoord = { ascii - 65, (coordArray[1] - '0')-1 };
+            System.out.println(Arrays.toString(numcoord)); */
+
+
+
             int col = Integer.parseInt(coord.substring(1)) - 1;
             int row= ((int) coord.charAt(0)) - 65;
 
-            int[] numcoord = {row+1,col}; 
+            int[] numcoord = {row,col}; 
 
             System.out.println(Arrays.toString(numcoord));
 
@@ -60,8 +68,129 @@ public class Main {
             } */
 
             
+
+
+
+
+
         }
+        //before game setup (menu)
+
+        //prompts user to load save or start a new game
+
+        System.out.println("complex (1) or simple (2) ai?");
+        int gameMode = input.nextInt();
+        //set the ai 
+        
+        System.out.println("Who goes first? you (1) or ai (2)?");
+        int firstMove = input.nextInt();
+
+        //turn gameplay loop
+        int enemyHits = 0;
+        int enemyMisses = 0;
+        int ourHits = 0;
+        int ourMisses = 0;
+
+        Board ourBoard = new Board(Constants.boardSize);
+        //place 5 ships on ourboard
+        Board enemyBoard = new Board(Constants.boardSize);
+
+        //while enemyhits or ourhits are less than 17
+        while (enemyHits<17||ourHits<17){
+
+            ourBoard.drawBoard(ourBoard);
+
+            //player turn
+            if (firstMove == 1){
+                System.out.println("Enter a coordiante: letter number");
+                String coord = input.nextLine().toUpperCase();
+
+                int col = Integer.parseInt(coord.substring(1)) - 1;
+                int row= ((int) coord.charAt(0)) - 65;
+
+                int[] numcoord = {row,col}; 
+
+                System.out.println("You attaked"+Arrays.toString(numcoord));
+
+                //if player hit
+                //if numcoord coord on the ourboard is not deflaut 
+                if (!ourBoard.getPoint(numcoord[0],numcoord[1]).getShipId().equals("deflaut")){
+                    //call checkIfHit
+                   //check which ship is hit and which ship is 
+                   //tell user, which ship is hit
+                   System.out.println("YOU HIT MY"+ourBoard.getPoint(numcoord[0],numcoord[1]).getShipId());
+                   enemyHits++;
+
+                  //if sunk
+                  //call check if sunk
+                    //if shipIsSunk is true{
+                        //System.out.println("YOU"VE SUNK MY"+shipId);
+                    //}
+
+                //if player miss
+                }else{
+                    System.out.println("Missed ;)");
+                    enemyMisses++;
+                }
+            }
+            //prompt user to save, or save and exit
+
+
+            // ai turn 
+            else{
+                //int[] numcoord = call attack method and return an attack coordinate
+                //System.out.println("AI's attack"+Arrays.toString(numcoord));
+
+                System.out.println("hit(1) or miss(2)?");
+                int userinput = input.nextInt(); 
+
+                    //if ai hit
+                    if (userinput == 1){
+
+                        System.out.println("Which ship?");
+                        String [] theShips= {"Carrier","BattleShip","Cruiser","Submarine","Destroyer"};
+
+                        //prints shipIDs alongside numbers
+                        for (int i = 0; i<theShips.length;i++){
+                            System.out.println((i+1)+" "+theShips[i]);
+                        }
+                        userinput = input.nextInt();
+                        //updating the enemyboard 
+                        //enemyBoard.getPoint(numcoord[0],numcoord[1]).setShipId(theShips[userinput-1]);
+                        //enemyBoard.getPoint(numcoord[0],numcoord[1]).setIsHit(true));
+                        //add point to shipPoints 
+
+                        ourHits++;
+
+                        //ask if ship that was hit, has suck
+                        System.out.println("Not sunk(1) or has sunk(2)?");
+                        userinput = input.nextInt();
+                            
+                            //if sunk
+                            if (userinput == 2){
+                                //set all points of said enemyShip to sunk     
+                            }
+                
+                    //if ai miss
+                    }else {
+                        //enemyBoard.getPoint(numcoord[0],numcoord[1]).setIsTaken(true));
+                        ourMisses++;
+                        
+                    }
+
+
+                }
+            }// end of while gameplay loop
+
+            
+                
+
+
+    
+            
+        }
+
 
     }
 
-}
+
