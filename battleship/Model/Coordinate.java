013fcef107c7d91f6ddc50392fd5d0992f92a61c
Amber Liu <amber.sj.liu@gmail.com>
2021-12-14 13:27:00 -0500

    ??

---
+++ battleship/Model/Coordinate.java
@@ -0,0 +1,26 @@
+/* package battleship.Model;
+
+public class Coordinate {
+
+    public Coordinate() {
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Coordinate)) {
+            return false;
+        }
+        
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode);
+    }
+    
+    
+}
+ */
